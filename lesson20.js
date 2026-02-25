b77d44af1462cb76c82051ca4fe418e12cd9bc0a
Misha260578 <vara26578@gmail.com>
2026-02-25 19:11:02 +0300

    upgrade project

--- lesson20.js
+++ lesson20.js
@@ -1,20 +1,59 @@
 "use strict"
+
+const dataBase = {
+    userNameDb: [],
+    userPriceDb: []
+
+}
+
+function rander(){
+    nameContainer.innerHTML = ""
+    dataBase.userNameDb.forEach((e, i) => {
+    if (e) {
+        const pName = document.createElement("p")
+        pName.textContent = e
+        nameContainer.append(pName)
+    
+    }
+   
+    })
+    
+    titleContainer.innerHTML = ""
+    dataBase.userPriceDb.forEach((e, i) => {
+     if (e) {
+        const pTitle = document.createElement("p")
+        pTitle.textContent = e
+        titleContainer.append(pTitle)
+    
+    }
+    })
+}
+
+
+
 const btn = document.querySelector("button"),
     title = document.querySelector("#title"),
-    aname = document.querySelector("#name");
+    aname = document.querySelector("#name"),
+    titleContainer = document.querySelector(".title"),
+    nameContainer = document.querySelector(".name");
 
 
 btn.addEventListener("click",  ()=>{
-    const pTitle = document.createElement("p"),
-     pName = document.createElement("p"),
-    titleContainer = document.querySelector(".title"),
-     nameContainer = document.querySelector(".name")
-    pTitle.textContent = title.value
-    pName.textContent = aname.value
-    titleContainer.append(pTitle)
-    nameContainer.append(pName)
-    title.value = ''
-    aname.value = ''
+    if (title.value && aname.value) {
+        dataBase.userPriceDb.push(title.value)
+        dataBase.userNameDb.push(aname.value)
+    }
+    // const pTitle = document.createElement("p"),
+    //  pName = document.createElement("p"),
+    // titleContainer = document.querySelector(".title"),
+    // nameContainer = document.querySelector(".name")
+    // pTitle.textContent = title.value
+    // pName.textContent = aname.value
+    // titleContainer.append(pTitle)
+    // nameContainer.append(pName)
+    // title.value = ''
+    // aname.value = ''
+    rander()
 })
 
 
